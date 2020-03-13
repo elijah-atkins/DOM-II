0d3571c7ffd68a658229c852c75bfc916dd77d27
Elijah Atkins <elijahorama@gmail.com>
2020-03-12 21:22:26 -0600

    added banner mouseover animation

--- js/index.js
+++ js/index.js
@@ -1,1 +1,10 @@
 // Your code goes here
+const banner = document.querySelector(".intro img");
+banner.addEventListener('mouseenter', ()=>{
+    banner.style.transform = "scale(1.2)"
+    banner.style.transition = "all 0.3s"
+})
+banner.addEventListener('mouseleave', () =>{
+    banner.style.transform = "scale(1)"
+    banner.style.transition = "all 0.3s"
+})
